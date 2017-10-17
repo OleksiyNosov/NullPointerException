4adc73c5047ad0ba1d480b8c03e99e42d4a13c9d
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-17 17:17:07 +0300

    Create tests for Answers model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to be_an ApplicationRecord }
+
+  it { is_expected.to belong_to :question }
+
+  it { is_expected.to validate_presence_of :body }
 end

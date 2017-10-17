6cd7fdad8146cccb8b780577a79c72702d76ba4e
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-17 17:16:51 +0300

    Create tests for Questions model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { is_expected.to be_an ApplicationRecord }
+
+  it { is_expected.to have_many :answers }
+
+  it { is_expected.to validate_presence_of :title }
 end

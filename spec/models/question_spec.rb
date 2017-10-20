63d2f8e3d69b2c628c54739426cb057821feda8f
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-20 13:49:01 +0300

    RSpec Question model add test for answers dependent destroy

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { is_expected.to be_an ApplicationRecord }
 
-  it { is_expected.to have_many :answers }
+  it { is_expected.to have_many(:answers).dependent :destroy }
 
   it { is_expected.to validate_presence_of :title }
 end

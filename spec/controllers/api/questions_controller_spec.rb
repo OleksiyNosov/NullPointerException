e437e9e74b495dec9d5b336ef1910074dbe1ae11
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-24 03:06:31 +0300

    Create test for QuestionController for show and create

---
+++ spec/controllers/api/questions_controller_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe Api::QuestionsController, type: :controller do
+  it { is_expected.to be_an ApplicationController }
+
+  let(:attributes) { attributes_for(:question) }
+
+  let(:serialized_attributes) { attributes.stringify_keys }
+
+  let(:question) { instance_double(Question, id: 2, as_json: attributes, **attributes) }
+
+  let(:resource_class) { Question }
+
+  describe 'GET #show' do
+    context 'question exist' do
+      before { expect(subject).to receive(:resource).and_return question }
+
+      before { get :show, params: { id: question.id }, format: :json }
+
+      it 'returns status 200' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'returns question' do
+        expect(response_body).to eq serialized_attributes
+      end
+    end
+
+    # TODO: context 'question is not exist'
+  end
+
+  describe 'POST #create' do
+    before { expect(subject).to receive(:resource_class).and_return resource_class }
+
+    before { expect(resource_class).to receive(:new).with(permit! attributes).and_return question }
+
+    before { expect(question).to receive(:save) }
+
+    context 'question was created' do
+      before { expect(question).to receive(:valid?).and_return true }
+
+      before { post :create, params: { question: attributes }, format: :json }
+
+      it 'returns status 201' do
+        expect(response).to have_http_status 201
+      end
+
+      it 'returns question' do
+        expect(response_body).to eq serialized_attributes
+      end
+    end
+
+    context 'question was not created' do
+      before { expect(question).to receive(:valid?).and_return false }
+
+      before { expect(question).to receive(:errors).and_return :errors }
+
+      before { post :create, params: { question: attributes }, format: :json }
+
+      it 'returns status 422' do
+        expect(response).to have_http_status 422
+      end
+
+      it 'returns errors' do
+        expect(response_body).to eq 'errors'
+      end
+    end
+  end
+end

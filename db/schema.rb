6e614fb6f68543072b74bce71d3608b0c2364284
OleksiyNosov <oleksiynosov@gmail.com>
2017-10-24 07:40:38 +0300

    Add tags to Question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017123642) do
+ActiveRecord::Schema.define(version: 20171024043302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20171017123642) do
     t.text 'body'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.string 'tags', default: [], array: true
   end
 
   add_foreign_key 'answers', 'questions'

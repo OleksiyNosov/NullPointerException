11032b09ab7354750de5fa19379f90b69b2098ce
Oleksiy Nosov <nosov@mlsdev.com>
2017-11-29 16:27:10 +0200

    Add check for uniquness for token column in sessions

--- db/migrate/20171128155047_create_sessions.rb
+++ db/migrate/20171128155047_create_sessions.rb
@@ -1,10 +1,12 @@
 class CreateSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :sessions do |t|
-      t.string :token, index: true, unique: true
+      t.string :token, default: false
       t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :sessions, :token, unique: true
   end
 end

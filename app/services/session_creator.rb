f803c2dc6a16d2f5614c7adafea4a8b80b0a1fba
Oleksiy Nosov <nosov@mlsdev.com>
2017-11-29 16:39:08 +0200

    Create SessionCreator

---
+++ app/services/session_creator.rb
@@ -0,0 +1,18 @@
+class SessionCreator < ResourceCrudWorker
+  def initialize params
+    @email = params[:email]
+
+    @password = params[:password]
+  end
+
+  private
+  def process_action
+    user = User.find_by email: @email
+
+    @resource = Session.new user: user, token: SecureRandom.base64(64)
+
+    @resource.errors.add :password, 'is invalid' unless user.authenticate @password
+
+    @resource.save if @resource.errors.count.zero?
+  end
+end

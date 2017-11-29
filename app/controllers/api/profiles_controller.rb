18c6476cc5595aae49d63434f9673afce58aa2e1
Oleksiy Nosov <nosov@mlsdev.com>
2017-11-29 16:36:31 +0200

    Create profile controller

---
+++ app/controllers/api/profiles_controller.rb
@@ -0,0 +1,16 @@
+class Api::ProfilesController < ApplicationController
+  skip_before_action :authenticate, only: :create
+
+  private
+  def resource_class
+    User
+  end
+
+  def resource_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def resource
+    current_user
+  end
+end

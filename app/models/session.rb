e418e4205f6f16d49efea2d4ea95bf374a8b0b0a
Oleksiy Nosov <nosov@mlsdev.com>
2017-11-30 10:53:17 +0200

    Add password validation for session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,12 @@
 class Session < ApplicationRecord
   belongs_to :user
+
+  attr_accessor :password
+
+  validate :authenticate
+
+  private
+  def authenticate
+    errors.add :password, 'is invalid' unless user&.authenticate password
+  end
 end

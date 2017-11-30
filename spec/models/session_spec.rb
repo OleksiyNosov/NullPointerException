9e2cf25da3caa1c7dd16eaa394689be804e49340
Oleksiy Nosov <nosov@mlsdev.com>
2017-11-30 11:02:26 +0200

    Add test for session password validation

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -3,7 +3,30 @@ require 'rails_helper'
 RSpec.describe Session, type: :model do
   it { is_expected.to belong_to :user }
 
-  it { is_expected.to validate_presence_of :user }
+  describe '#authenticate' do
+    let(:password) { SecureRandom.base64(16) }
 
-  it { is_expected.to validate_presence_of :token }
+    let(:user) { FactoryGirl.create(:user, password: password) }
+
+    context 'password is valid' do
+      before { allow(user).to receive(:authenticate).with(password).and_return false }
+
+      it { expect { subject.send :authenticate }.to_not raise_error }
+    end
+
+    context 'password is not valid' do
+      before { allow(user).to receive(:authenticate).with(password).and_return true }
+
+      before do
+        #
+        # => errors.add
+        #
+        expect(subject).to receive(:errors) do
+          double.tap { |errors| expect(errors).to receive(:add).with(:password, 'is invalid') }
+        end
+      end
+
+      it { expect { subject.send :authenticate }.to_not raise_error }
+    end
+  end
 end

e5509bf2ebc5c056ed9147b6e6d1f8f3eb84c96b
Oleksiy Nosov <nosov@mlsdev.com>
2017-11-30 10:54:17 +0200

    Add sign_in module for tests

---
+++ spec/support/autheticate.rb
@@ -0,0 +1,9 @@
+module Authentication
+  def sign_in user = nil
+    user ||= User.new
+
+    allow(controller).to receive(:authenticate).and_return user
+
+    allow(controller).to receive(:current_user).and_return user
+  end
+end

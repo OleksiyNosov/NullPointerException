34390373195e8b88ced761016917d354d3404621
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-01 13:06:38 +0200

    Add authentication with password to specs support

--- spec/support/autheticate.rb
+++ spec/support/autheticate.rb
@@ -6,4 +6,12 @@ module Authentication
 
     allow(controller).to receive(:current_user).and_return user
   end
+
+  def sign_in_with_password user = nil
+    user ||= User.new
+
+    allow(controller).to receive(:authenticate_with_password).and_return user
+
+    allow(controller).to receive(:current_user).and_return user
+  end
 end

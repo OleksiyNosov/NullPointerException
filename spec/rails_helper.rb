a05dbfe4117d12f528bd47ca454d409ec3831731
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-04 15:00:01 +0200

    Update code style due to new rubocop configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,10 +10,10 @@ require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{ ::Rails.root }/spec/fixtures"
 
   config.use_transactional_fixtures = true
 

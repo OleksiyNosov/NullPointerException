35965641e6ea2a4aa02285c5f566a34ef836b4da
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-05 17:13:00 +0200

    Update email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  include EmailValidations
-
   has_secure_password
 
   has_many :sessions
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end

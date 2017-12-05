22f65e141084ce7d6d4caf1c59f0bc16c3d0827f
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-05 17:18:37 +0200

    Some style changes

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -1,11 +1,11 @@
 module Authenticatable
   attr_reader :current_user
 
-  private
   def self.included(base)
     base.before_action :authenticate_with_token
   end
 
+  private
   def authenticate_with_token
     token, _options = ActionController::HttpAuthentication::Token.token_and_options(request)
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :sessions
 
   validates :email,
-    presence: true,
-    uniqueness: { case_sensitive: false },
-    length: { maximum: 255 },
-    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end

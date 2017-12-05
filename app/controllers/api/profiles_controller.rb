9cbebc214271b8af3f44cd44a5b760967b7649f4
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-05 15:24:52 +0200

    Update profile and user controllers due to changes in routes

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,19 +1,5 @@
 class Api::ProfilesController < ApplicationController
-  skip_before_action :authenticate_with_token, only: :create
-
   private
-  def resource_class
-    User
-  end
-
-  def resource_updator
-    ProfileUpdator.new resource, resource_params
-  end
-
-  def resource_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
-
   def resource
     current_user
   end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,8 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :authenticate_with_token, only: :create
+
+  private
+  def resource_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end

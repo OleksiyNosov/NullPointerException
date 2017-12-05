5963ad63ba63177bd4f5650b3f0829748d240267
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-05 15:21:52 +0200

    Add tests for email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,19 @@ RSpec.describe User, type: :model do
 
   it { is_expected.not_to allow_value('test').for(:email) }
 
+  it { is_expected.not_to allow_value('test@test').for(:email) }
+
+  it { is_expected.not_to allow_value('test@test.').for(:email) }
+
+  it { is_expected.not_to allow_value('test@test.x').for(:email) }
+
+  it { is_expected.not_to allow_value('t@t.a').for(:email) }
+
+  it { is_expected.not_to allow_value('test@.com').for(:email) }
+
+  it { is_expected.not_to allow_value('@a.com').for(:email) }
+
+  it { is_expected.not_to allow_value('..@..com').for(:email) }
+
   it { is_expected.to allow_value('test@example.com').for(:email) }
 end

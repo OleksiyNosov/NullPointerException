3a60c857dc5523938827b0a0ae1bf79e4697687a
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-05 15:38:48 +0200

    Update tests for profile and users controllers

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -7,7 +7,9 @@ RSpec.describe Api::UsersController, type: :controller do
 
   let(:serialized_attributes) { attributes.stringify_keys }
 
-  let(:user) { instance_double(User, id: 5, as_json: attributes, **attributes) }
+  let(:user) { instance_double User, id: 5, as_json: attributes, **attributes }
+
+  let(:resource_class) { User }
 
   describe 'GET #index' do
     before { sign_in }
@@ -50,6 +52,102 @@ RSpec.describe Api::UsersController, type: :controller do
       before { get :show, params: { id: user.id }, format: :json }
 
       it('returns status 404') { expect(response).to have_http_status 404 }
+    end
+  end
+
+  describe 'POST #create' do
+    before { expect(subject).to receive(:resource_class).and_return resource_class }
+
+    before { expect(resource_class).to receive(:new).with(permit! attributes).and_return user }
+
+    before { expect(user).to receive(:save) }
+
+    context 'new user was created' do
+      before do
+        #
+        # => user.errors.empty?
+        #
+        expect(user).to receive(:errors) do
+          double.tap { |errors| expect(errors).to receive(:empty?).and_return true }
+        end
+      end
+
+      before { allow(user).to receive(:valid?).and_return true }
+
+      before { post :create, params: { user: attributes }, format: :json }
+
+      it('returns status 201') { expect(response).to have_http_status 201 }
+
+      it('returns user profile') { expect(response_body).to eq serialized_attributes }
+    end
+
+    context 'new user was not created' do
+      before do
+        #
+        # => user.errors.empty?
+        #
+        expect(user).to receive(:errors) do
+          double.tap { |errors| expect(errors).to receive(:empty?).and_return false }
+        end
+      end
+
+      before { allow(user).to receive(:valid?).and_return false }
+
+      before { expect(user).to receive(:errors).and_return :errors }
+
+      before { post :create, params: { user: attributes }, format: :json }
+
+      it('returns status 422') { expect(response).to have_http_status 422 }
+
+      it('returns errors') { expect(response_body).to eq 'errors' }
+    end
+  end
+
+  describe 'PATCH $update' do
+    before { sign_in }
+
+    before { expect(subject).to receive(:resource).and_return user }
+
+    before { expect(user).to receive(:update).with(permit! attributes) }
+
+    describe 'user profile was updated' do
+      before do
+        #
+        # => user.errors.empty?
+        #
+        expect(user).to receive(:errors) do
+          double.tap { |errors| expect(errors).to receive(:empty?).and_return true }
+        end
+      end
+
+      before { allow(user).to receive(:valid?).and_return true }
+
+      before { patch :update, params: { id: user.id, user: attributes }, format: :json }
+
+      it('returns status 200') { expect(response).to have_http_status 200 }
+
+      it('returns updated profile') { expect(response_body).to eq serialized_attributes }
+    end
+
+    describe 'user profile was not updated' do
+      before do
+        #
+        # => user.errors.empty?
+        #
+        expect(user).to receive(:errors) do
+          double.tap { |errors| expect(errors).to receive(:empty?).and_return false }
+        end
+      end
+
+      before { allow(user).to receive(:valid?).and_return false }
+
+      before { expect(user).to receive(:errors).and_return :errors }
+
+      before { patch :update, params: { id: user.id, user: attributes }, format: :json }
+
+      it('returns status 422') { expect(response).to have_http_status 422 }
+
+      it('returns errors') { expect(response_body).to eq 'errors' }
     end
   end
 

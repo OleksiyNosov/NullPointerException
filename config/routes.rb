b9c7379c3c569010a4a300a007f0a88d962e4869
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-05 15:21:04 +0200

    Udpate profile and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
 
     resources :answers, only: %i[index show create update destroy]
 
-    resources :users, only: %i[index show]
+    resources :users, only: %i[index show create show update]
 
-    resource :profile, only: %i[show create update]
+    resource :profile, only: %i[show]
 
     resources :sessions, only: %i[index show create destroy]
   end

e69a39ef8c284ed7a0e26d741ac8fd167defe55b
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-11 14:00:14 +0200

    Delete session code and tests

--- spec/serializers/session_serializer_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SessionSerializer do
-  subject { SessionSerializer.new Session.new user: create(:user) }
-
-  let(:attributes) { subject.attributes.keys }
-
-  it { expect(attributes).to eq %i[token] }
-end

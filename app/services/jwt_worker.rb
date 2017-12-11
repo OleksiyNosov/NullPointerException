fbc27e527af1891e233dbe01aada90621e74ad72
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-11 13:50:52 +0200

    Create service for encoding and decoding tokens

---
+++ app/services/jwt_worker.rb
@@ -0,0 +1,19 @@
+class JwtWorker
+  ALGORITHM = 'HS256'.freeze
+  HEADERS = { typ: 'JWT', alg: ALGORITHM }
+  SECRET_KEY = Rails.application.secrets.secret_key_base
+
+  private_constant :ALGORITHM, :HEADERS, :SECRET_KEY
+
+  class << self
+    def encode payload
+      JWT.encode payload, SECRET_KEY, ALGORITHM, HEADERS
+    end
+
+    def decode token
+      JWT.decode token, SECRET_KEY, true, algorithm: ALGORITHM
+    rescue JWT::ExpiredSignature, JWT::DecodeError
+      false
+    end
+  end
+end

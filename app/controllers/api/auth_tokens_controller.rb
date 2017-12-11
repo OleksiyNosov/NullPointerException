0f9c1934971a60a2f4d4a0cc1bb03b8525a186a5
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-11 13:51:37 +0200

    Create controller for creation of auth tokens

---
+++ app/controllers/api/auth_tokens_controller.rb
@@ -0,0 +1,18 @@
+class AuthTokensController < ActionController::API
+  def create
+    if user&.authenticate params[:password]
+      render json: { token: new_token }, status: 201
+    else
+      render json: { errors: 'email or password is invalid' }, status: 422
+    end
+  end
+
+  private
+  def new_token
+    JwtWorker.encode user_id: user.id, exp: 7.days.from_now.to_i
+  end
+
+  def user
+    @user ||= User.find_by email: params[:email]
+  end
+end

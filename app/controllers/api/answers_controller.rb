1f72e72892db50ed909010501d1a81bff6fa1fdc
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-12 17:50:51 +0200

    Make quesions and answers resources accesible without sign in

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,4 +1,6 @@
 class Api::AnswersController < ApplicationController
+  skip_before_action :authenticate, only: %i[index show]
+
   private
   def resource_params
     params.require(:answer).permit(:question_id, :body)
--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,4 +1,6 @@
 class Api::QuestionsController < ApplicationController
+  skip_before_action :authenticate, only: %i[index show]
+
   private
   def resource_params
     params.require(:question).permit(:title, :body, tags: [])

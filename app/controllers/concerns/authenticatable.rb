1ee0152225e82f6c6bb9d95e3d46d00eb61bd29b
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-12 09:39:47 +0200

    Rewrite authenticate method to be more simple

--- app/controllers/concerns/authenticatable.rb
+++ app/controllers/concerns/authenticatable.rb
@@ -12,15 +12,7 @@ module Authenticatable
     authenticate_or_request_with_http_token do |token, options|
       @decoded_token = JwtWorker.decode token
 
-      if @decoded_token && user_from_token_data
-        @current_user = user_from_token_data
-      else
-        false
-      end
+      @current_user = User.find @decoded_token.first['user_id'] if @decoded_token
     end
-  end
-
-  def user_from_token_data
-    @user_from_token_data ||= User.find @decoded_token.first['user_id']
   end
 end

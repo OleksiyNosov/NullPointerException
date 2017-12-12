f8f0efb740c06e6a7edf6bb04bef1ce279f7fb8d
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-12 15:40:24 +0200

    Delete useless code in JwtWorker

--- app/services/jwt_worker.rb
+++ app/services/jwt_worker.rb
@@ -1,16 +1,15 @@
 class JwtWorker
-  ALGORITHM = 'HS256'
   SECRET_KEY = Rails.application.secrets.secret_key_base
 
   class << self
     def encode payload
       payload[:exp] ||= 7.days.from_now.to_i
 
-      JWT.encode payload, SECRET_KEY, ALGORITHM
+      JWT.encode payload, SECRET_KEY
     end
 
     def decode token
-      JWT.decode token, SECRET_KEY, true, algorithm: ALGORITHM
+      JWT.decode token, SECRET_KEY
     rescue JWT::ExpiredSignature, JWT::DecodeError
       false
     end

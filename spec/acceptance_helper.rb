030008e50884b3c31cb54b5d3afbae06282461f9
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-13 13:25:00 +0200

    Remove useless code in acceptance_helper

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -10,11 +10,7 @@ require 'rspec/rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
-
 RSpec.configure do |config|
-  config.fixture_path = "#{ ::Rails.root }/spec/fixtures"
-
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
@@ -22,10 +18,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods
-
-  config.include JsonResposeBodyParser
-
-  config.include Permitter
 end
 
 Shoulda::Matchers.configure do |config|

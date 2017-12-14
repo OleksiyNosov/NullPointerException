fe03f9e867f583021df36a6a98c165180a03c13a
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-14 09:38:08 +0200

    Remove useless tests from application_controller_spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,37 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
-  it { is_expected.to be_kind_of Authenticatable }
+  it { is_expected.to be_an ActionController::API }
+
+  it { is_expected.to be_kind_of Authenticationalbe }
 
   it { is_expected.to be_kind_of Resourceable }
-
-  describe '#resource' do
-    let(:resource) { double }
-
-    let(:id) { 1 }
-
-    let(:resource_class) { double }
-
-    before do
-      expect(subject).to receive(:params) do
-        double.tap { |params| expect(params).to receive(:[]).with(:id).and_return id }
-      end
-    end
-
-    before do
-      expect(subject).to receive(:resource_class) do
-        double.tap { |resource_class| expect(resource_class).to receive(:find).with(id).and_return resource }
-      end
-    end
-
-    its(:resource) { is_expected.to eq resource }
-  end
-
-  describe '#resource_class' do
-    before { allow(controller).to receive_message_chain(:class, :controller_name).and_return 'fake_classes' }
-
-    before { class FakeClass end }
-
-    its(:resource_class) { is_expected.to eq FakeClass }
-  end
 end

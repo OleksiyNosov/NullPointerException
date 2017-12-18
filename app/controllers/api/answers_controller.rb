77bf5ae56b3522afae5bc24206ef40302d6fc020
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-18 16:27:51 +0200

    Change style in controllers actions

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -2,24 +2,21 @@ class Api::AnswersController < ApplicationController
   skip_before_action :authenticate, only: :index
 
   def create
-    ResourceCreator
-      .new(Answer, resource_params)
+    ResourceCreator.new(Answer, resource_params)
       .on(:succeeded) { |resource| render json: resource, status: 201 }
       .on(:failed) { |errors| render json: errors, status: 422 }
       .call
   end
 
   def update
-    ResourceUpdator
-      .new(resource, resource_params)
+    ResourceUpdator.new(resource, resource_params)
       .on(:succeeded) { |resource| render json: resource }
       .on(:failed) { |errors| render json: errors, status: 422 }
       .call
   end
 
   def destroy
-    ResourceDestroyer
-      .new(resource)
+    ResourceDestroyer.new(resource)
       .on(:succeeded) { head 204 }
       .on(:failed) { |errors| render json: errors, status: 422 }
       .call
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,16 +2,14 @@ class Api::UsersController < ApplicationController
   skip_before_action :authenticate, only: :create
 
   def create
-    ResourceCreator
-      .new(User, resource_params)
+    ResourceCreator.new(User, resource_params)
       .on(:succeeded) { |resource| render json: resource, status: 201 }
       .on(:failed) { |errors| render json: errors, status: 422 }
       .call
   end
 
   def update
-    ResourceUpdator
-      .new(resource, resource_params)
+    ResourceUpdator.new(resource, resource_params)
       .on(:succeeded) { |resource| render json: resource }
       .on(:failed) { |errors| render json: errors, status: 422 }
       .call

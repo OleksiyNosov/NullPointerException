cfa8756ef63eb8d6a80b02942275f2ad75dd3bff
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-19 14:39:08 +0200

    Update tests for users controller

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -5,39 +5,39 @@ RSpec.describe Api::UsersController, type: :controller do
 
   it { is_expected.to be_kind_of Authenticatable }
 
-  let(:attributes) { attributes_for(:user) }
+  let(:attributes) { user.attributes }
 
-  let(:serialized_attributes) { attributes.stringify_keys }
+  let(:invalid_attributes) { attributes.merge('password' => '123', 'password_confirmation' => '123','email' => '') }
 
-  let(:user) { instance_double User, id: 5, as_json: attributes, **attributes }
+  let(:errors) { { 'email' => ["can't be blank", 'is invalid'] }.to_json }
 
-  let(:permitted_attributes) { ActionController::Parameters.new(attributes).permit! }
+  let(:user) { FactoryBot.create(:user, password: '123') }
+
+  let(:serialized_user) { UserSerializer.new(user) }
+
+  let(:serialized_user_json) { serialized_user.to_json }
 
   describe 'POST #create' do
-    before { allow(User).to receive(:new).with(permitted_attributes).and_return user }
-
-    before { expect(user).to receive(:save) }
-
     context 'new user was created' do
-      before { allow(user).to receive(:valid?).and_return true }
+      let(:permitted_attributes) { ActionController::Parameters.new(email: user.email).permit! }
+
+      let(:serialized_attributes) { serialized_user.to_h.without(:id).stringify_keys }
+
+      before { allow(User).to receive(:new).with(permitted_attributes).and_return user }
 
       before { post :create, params: { user: attributes }, format: :json }
 
       it('returns status 201') { expect(response).to have_http_status 201 }
 
-      it('returns user profile') { expect(response_body).to eq serialized_attributes }
+      it('returns user profile') { expect(JSON.parse response.body).to include serialized_attributes }
     end
 
     context 'new user was not created' do
-      before { allow(user).to receive(:valid?).and_return false }
-
-      before { allow(user).to receive(:errors).and_return :errors }
-
-      before { post :create, params: { user: attributes }, format: :json }
+      before { post :create, params: { user: invalid_attributes }, format: :json }
 
       it('returns status 422') { expect(response).to have_http_status 422 }
 
-      it('returns errors') { expect(response_body).to eq 'errors' }
+      it('returns errors') { expect(response.body).to eq errors }
     end
   end
 
@@ -46,61 +46,47 @@ RSpec.describe Api::UsersController, type: :controller do
 
     describe 'GET #index' do
       context 'users exist' do
-        before { allow(subject).to receive(:collection).and_return [user] }
+        before { allow(subject).to receive(:collection).and_return :collection }
 
         before { get :index, format: :json }
 
         it('returns status 200') { expect(response).to have_http_status 200 }
 
-        it('returns users') { expect(response_body).to eq [serialized_attributes] }
+        it('returns users') { expect(response.body).to eq :collection.to_json }
       end
     end
 
     describe 'GET #show' do
       context 'user exist' do
-        before { allow(subject).to receive(:resource).and_return user }
-
         before { get :show, params: { id: user.id }, format: :json }
 
         it('returns status 200') { expect(response).to have_http_status 200 }
 
-        it('returns user') { expect(response_body).to eq serialized_attributes }
+        it('returns user') { expect(response.body).to eq serialized_user_json }
       end
 
       context 'user not exist' do
-        before { expect(subject).to receive(:resource).and_raise ActiveRecord::RecordNotFound }
-
-        before { get :show, params: { id: user.id }, format: :json }
+        before { get :show, params: { id: -1 }, format: :json }
 
         it('returns status 404') { expect(response).to have_http_status 404 }
       end
     end
 
     describe 'PATCH $update' do
-      before { allow(subject).to receive(:resource).and_return user }
-
-      before { expect(user).to receive(:update).with(permitted_attributes) }
-
       describe 'user profile was updated' do
-        before { allow(user).to receive(:valid?).and_return true }
-
         before { patch :update, params: { id: user.id, user: attributes }, format: :json }
 
         it('returns status 200') { expect(response).to have_http_status 200 }
 
-        it('returns updated profile') { expect(response_body).to eq serialized_attributes }
+        it('returns updated profile') { expect(response.body).to eq serialized_user_json }
       end
 
       describe 'user profile was not updated' do
-        before { allow(user).to receive(:valid?).and_return false }
-
-        before { allow(user).to receive(:errors).and_return :errors }
-
-        before { patch :update, params: { id: user.id, user: attributes }, format: :json }
+        before { patch :update, params: { id: user.id, user: invalid_attributes }, format: :json }
 
         it('returns status 422') { expect(response).to have_http_status 422 }
 
-        it('returns errors') { expect(response_body).to eq 'errors' }
+        it('returns errors') { expect(response.body).to eq errors }
       end
     end
   end

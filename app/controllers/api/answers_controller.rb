a6933182eafe705a1fca726fd1fbc03cfdf99225
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-20 15:39:33 +0200

    Update logic of answer creation

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -2,7 +2,7 @@ class Api::AnswersController < ApplicationController
   skip_before_action :authenticate, only: :index
 
   def create
-    ResourceCreator.new(Answer, resource_params)
+    AnswerCreator.new(resource_params)
       .on(:succeeded) { |resource| render json: resource, status: 201 }
       .on(:failed) { |errors| render json: errors, status: 422 }
       .call
@@ -32,6 +32,6 @@ class Api::AnswersController < ApplicationController
   end
 
   def collection
-    @collection ||= Answer.all
+    @collection ||= Question.find(params[:question_id]).answers
   end
 end

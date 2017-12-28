447cae0a79228118c5835259c469b4d1caf55b6e
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-28 14:24:40 +0200

    Add tests for broadcast_resource in services

--- spec/services/resource_creator_spec.rb
+++ spec/services/resource_creator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ResourceCreator do
 
   let(:resource_class) { Question }
 
-  let(:resource) { instance_double(resource_class, **resource_attributes) }
+  let(:resource) { instance_double(resource_class, valid?: true ,**resource_attributes) }
 
   subject { ResourceCreator.new resource_class, resource_attributes }
 
@@ -25,5 +25,29 @@ RSpec.describe ResourceCreator do
     before { expect(subject).to receive(:broadcast_resource) }
 
     it('create and broadcast resource') { expect { subject.call }.to_not raise_error }
+  end
+
+  describe  '#broadcast_resource' do
+    context 'when resource is valid' do
+      before { allow(subject).to receive_message_chain(:resource, :valid?).and_return true }
+
+      before { allow(subject).to receive(:resource).and_return resource }
+
+      before { expect(subject).to receive(:broadcast).with(:succeeded, resource) }
+
+      it('broadcast resource') { expect { subject.send :broadcast_resource }.to_not raise_error }
+    end
+
+    context 'when resource is invalid' do
+      let(:resource_errors) { double }
+
+      before { allow(subject).to receive_message_chain(:resource, :valid?).and_return false }
+
+      before { allow(subject).to receive_message_chain(:resource, :errors).and_return resource_errors }
+
+      before { expect(subject).to receive(:broadcast).with(:failed, resource_errors) }
+
+      it('broadcast resource errors') { expect { subject.send :broadcast_resource }.to_not raise_error }
+    end
   end
 end
--- spec/services/resource_updator_spec.rb
+++ spec/services/resource_updator_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ResourceUpdator do
   let(:resource_attributes) { attributes_for(:question) }
 
-  let(:resource) { instance_double Question }
+  let(:resource) { instance_double Question, valid?: true }
 
   subject { ResourceUpdator.new resource, resource_attributes }
 
@@ -21,5 +21,29 @@ RSpec.describe ResourceUpdator do
     before { expect(subject).to receive(:broadcast_resource) }
 
     it('update and broadcast resource') { expect { subject.call }.to_not raise_error }
+  end
+
+  describe  '#broadcast_resource' do
+    context 'when resource is valid' do
+      before { allow(subject).to receive_message_chain(:resource, :valid?).and_return true }
+
+      before { allow(subject).to receive(:resource).and_return resource }
+
+      before { expect(subject).to receive(:broadcast).with(:succeeded, resource) }
+
+      it('broadcast resource') { expect { subject.send :broadcast_resource }.to_not raise_error }
+    end
+
+    context 'when resource is invalid' do
+      let(:resource_errors) { double }
+
+      before { allow(subject).to receive_message_chain(:resource, :valid?).and_return false }
+
+      before { allow(subject).to receive_message_chain(:resource, :errors).and_return resource_errors }
+
+      before { expect(subject).to receive(:broadcast).with(:failed, resource_errors) }
+
+      it('broadcast resource errors') { expect { subject.send :broadcast_resource }.to_not raise_error }
+    end
   end
 end

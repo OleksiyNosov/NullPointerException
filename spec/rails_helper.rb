1706a6505e53388c9b3b08743ff16409495b8136
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-28 17:47:08 +0200

    Remove useless include in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,8 +8,6 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
-require 'database_cleaner'
-
 ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }

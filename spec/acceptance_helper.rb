a46576ac1508f87e6d044eabf42b10b6a3fcea1c
Oleksiy Nosov <nosov@mlsdev.com>
2017-12-28 14:57:00 +0200

    Config rspec format output for acceptance tests

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -17,6 +17,8 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 
+  config.formatter = :documentation
+
   config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do

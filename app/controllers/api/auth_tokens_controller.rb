00e8e8cc15f846783c3036dacc0a3b8a4219b28e
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-03 10:19:14 +0200

    Fix auth token authorization

--- app/controllers/api/auth_tokens_controller.rb
+++ app/controllers/api/auth_tokens_controller.rb
@@ -3,11 +3,9 @@ class Api::AuthTokensController < ActionController::API
   include Pundit
 
   def create
-    user = User.find_by email: resource_params[:email]
+    authorize :AuthToken
 
-    authorize user
-
-    if user&.authenticate resource_params[:password]
+    if current_user&.authenticate resource_params[:password]
       render json: { token: JWTWorker.encode(user_id: user.id) }, status: 201
     else
       render json: { email: ['invalid email'], password: ['invalid password'] }, status: 422
@@ -17,5 +15,9 @@ class Api::AuthTokensController < ActionController::API
   private
   def resource_params
     params.require(:sign_in).permit(:email, :password)
+  end
+
+  def current_user
+    @current_user ||= User.find_by email: resource_params[:email]
   end
 end
--- app/policies/auth_token_policy.rb
+++ app/policies/auth_token_policy.rb
@@ -1,11 +1,5 @@
 class AuthTokenPolicy < ApplicationPolicy
-  attr_reader :user
-
-  def initialize user
-    @user = user
-  end
-
   def create?
-    user.email_confirmation
+    @user.email_confirmation
   end
 end

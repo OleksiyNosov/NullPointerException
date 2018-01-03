c1159476f69af59b4c3f7dc1111b3f5ffbe1a469
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-03 08:59:18 +0200

    Update user factorie for new user fields

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    email_confirmation { true }
+    confirmation_token { SecureRandom.urlsafe_base64.to_s }
   end
 end

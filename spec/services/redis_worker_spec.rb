34703144a53d9108dd14fc0322399d5e9f17afbd
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-05 17:36:38 +0200

    Create tests for redis worker

---
+++ spec/services/redis_worker_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe RedisWorker do
+  subject { RedisWorker }
+
+  let(:redis_double) { double }
+
+  describe '.instance' do
+    before { expect(Redis).to receive(:new).with(host: 'localhost', port: 6379).and_return redis_double }
+
+    it('returns instance of redis') { expect(subject.instance).to eq redis_double }
+  end
+end

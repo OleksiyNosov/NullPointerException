d1b59895fe0763242597d3f749752dcbdf402126
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-05 11:59:11 +0200

    Fix user creator

--- app/services/user_creator.rb
+++ app/services/user_creator.rb
@@ -6,6 +6,6 @@ class UserCreator < ResourceCrudWorker
   def process_action
     @resource = User.new @params
 
-    @resource.save && RegistrationMailer.deliver @resource
+    @resource.save && RegistrationMailer.deliver(@resource)
   end
 end

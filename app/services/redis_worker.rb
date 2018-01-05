c8abb93bbc757fde95ea476fb8d25e589098efee
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-05 11:58:31 +0200

    Fix redis worker

--- app/services/redis_worker.rb
+++ app/services/redis_worker.rb
@@ -1,7 +1,7 @@
 class RedisWorker
   class << self
     def instance
-      @@redis ||= Redis.new host: 'localhost', port: 6379
+      @redis ||= Redis.new host: 'localhost', port: 6379
     end
   end
 end

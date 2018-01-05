955c565f301b531ce64ec7a61c9ceeaa647a9281
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-05 12:00:08 +0200

    Add pundit configs in rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
+require 'pundit/rspec'
+
 ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }

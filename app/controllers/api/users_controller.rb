79a496d751b4e0ac9a76689307638b9858298ca1
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-08 10:14:08 +0200

    Add first and last name to required attributes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -29,7 +29,7 @@ class Api::UsersController < ApplicationController
 
   private
   def resource_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
   def resource

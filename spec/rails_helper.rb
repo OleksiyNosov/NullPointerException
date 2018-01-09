9c72c9014b9325246e198114c08402ca8f6bdb2c
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-09 15:18:22 +0200

    Delete useless configs in rspec for fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,10 +12,6 @@ ActiveRecord::Migration.maintain_test_schema!
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.fixture_path = "#{ ::Rails.root }/spec/fixtures"
-
-  config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods

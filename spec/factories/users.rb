33c8ee9cfe954cffeb993696f5a84b340a297538
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-09 15:20:24 +0200

    Update user factory to generate only unique emails

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    email { Faker::Internet.email }
+    sequence(:email) { |n| "user#{ n }@example.com" }
     password { Faker::Internet.password }
     status { :confirmed }
   end

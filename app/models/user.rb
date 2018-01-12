4c4ceab53eb341b139f5ac5a8720f672715b68d9
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-12 17:18:08 +0200

    Add scope to user for confirmed?

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { maximum: 255 },
             format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
+  scope :confirmed?, -> { not not_confirmed? }
 end

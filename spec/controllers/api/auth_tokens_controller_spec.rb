9f09930b87149d2c10f0fb8da17ce2ac6b587d21
Oleksiy Nosov <nosov@mlsdev.com>
2018-01-19 11:51:29 +0200

    Delte useless varibalse in auth token controller tests

--- spec/controllers/api/auth_tokens_controller_spec.rb
+++ spec/controllers/api/auth_tokens_controller_spec.rb
@@ -16,8 +16,6 @@ RSpec.describe Api::AuthTokensController, type: :controller do
   let(:token) { JWTWorker.encode user_id: user.id, intent: 'authentication' }
 
   describe 'POST #create' do
-    let(:errors_json) { { 'email' => ['invalid email'], 'password' => ['invalid password'] }.to_json }
-
     let(:params) { { sign_in: { email: email, password: password } } }
 
     context 'when email and password are valid' do
